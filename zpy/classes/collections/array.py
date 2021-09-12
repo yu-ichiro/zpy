a3d033618b2ac5f2164d94664ffb8b8733772619
Yuichiro Smith <contact@yu-smith.com>
2021-09-13 01:35:44 +0900

    :sparkles: add get to Array

--- zpy/classes/collections/array.py
+++ zpy/classes/collections/array.py
@@ -2,19 +2,15 @@ from itertools import chain
 from typing import Iterable, Callable, cast, Tuple, TypeVar
 
 from zpy.classes.bases import Applicative, Cartesian
+from zpy.classes.logical.maybe import Maybe, Nothing, Just
 from zpy.operators import builtin_map, builtin_product, builtin_reduce
 
 T = TypeVar("T")
 U = TypeVar("U")
 
 
-_empty = object()
-
-
 class Array(Applicative[T], Cartesian[T], list):
-    def __new__(cls, iter_: Iterable[T] = _empty):
-        if iter_ is _empty:
-            return list.__new__(cls)
+    def __new__(cls, iter_: Iterable[T] = ()):
         return list.__new__(cls, iter_)
 
     @classmethod
@@ -29,6 +25,10 @@ class Array(Applicative[T], Cartesian[T], list):
         cls = type(self)
         return cls(builtin_map(f, self))
 
+    def flat_map(self, f: Callable[[T], Iterable[U]]) -> "Array[U]":
+        cls = type(self)
+        return self.reduce(cls(), lambda a, item: cls(chain(a, f(item))))
+
     def product(self, f: "Array[U]") -> "Array[Tuple[T, U]]":
         cls = type(self)
         return cls(cast(Array[Tuple[T, U]], builtin_product(self, f)))
@@ -41,6 +41,14 @@ class Array(Applicative[T], Cartesian[T], list):
 
     def __repr__(self):
         return f"{type(self).__name__}({super().__repr__()})"
+
+    def get(self, idx: int) -> Maybe[T]:
+        if idx >= len(self):
+            return Nothing()
+        return Just(self[idx])
+
+    def __getitem__(self, item) -> T:
+        return list.__getitem__(self, item)
 
     def swap(self, x: int, y: int):
         self[x], self[y] = self[y], self[x]
